4823d35213ab4a37e0d72d1889125a5368eefbed
Siarhei Kvachonak <segrey1989@tut.by>
2018-10-06 13:12:02 +0300

    6 functions were made

--- src/index.js
+++ src/index.js
@@ -1,22 +1,61 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = (propertyName) => {this.propertyName = propertyName};
-const createNotEnumerableProperty = (propertyName) => {this.propertyName = propertyName;
-Object.defineProperty(this, propertyName, {
-  value:'undefined',
+const createEnumerableProperty = (propertyName) => {
+return  propertyName;
+};
+
+const createNotEnumerableProperty = (propertyName) => {
+  Object.defineProperty(Object.prototype, propertyName, {
+    value:'value',
   enumerable:false
-})};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+});
+ return propertyName;
+}
+
+
+const createProtoMagicObject = () => {
+return Function;
+};
+
+
+const incrementor = () => {
+incrementor.value;
+
+  const f = () => {
+    if(!incrementor.value) incrementor.value=0;
+
+   incrementor.value += 1;
+  return f;
+}
+ f.toString= ()=> incrementor.value;
+return f();
+};
+
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+
+const getDeepPropertiesCount = (obj) => {
+  let count = Object.keys(obj).length;
+	  for (let key in obj){
+	    if (typeof obj[key] === "object") {
+	      count += getDeepPropertiesCount(obj[key]);
+	    }
+
+	  }
+    return count;
+	}
+
+  const createSerializedObject = () => {};
 const toBuffer = () => {};
-const sortByProto = () => {};
+
+const sortByProto = (arr) => {
+let array = arr;
+array.sort((a,b) => a.__proto - b.__proto__);
+return array;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
